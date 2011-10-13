732965557f4554464e4909f227aa55462be7bf0f
Chris Hallendy <chris.hallendy@gmail.com>
2011-10-13 12:12:59 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DetroitRubySampleApp::Application.config.secret_token = '8245a1f3dcdafc1d988b0afe73fe330f3f2687ad8d84f3a8b1a9e161f6a38e0498f50ee0d8574340f088c48cba391be1a9bdd034418ded2f99db07cdd35d2e0f'
